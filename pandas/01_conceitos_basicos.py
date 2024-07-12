8d8b7c4ca8a0860bbd10d8f2a5581c1ecd5dab2f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-12 09:40:10 -0700

    feat: add faturamento por filial2

--- pandas/01_conceitos_basicos.py
+++ pandas/01_conceitos_basicos.py
@@ -4,3 +4,6 @@ import pandas as pd
 pasta_atual = Path(__file__).parent
 df_data = pd.read_csv(pasta_atual / 'supermarket_sales.csv')
 
+# 1. Qual o faturamento total por filial
+faturamento_total_por_filial = df_data.groupby('City')['Total'].sum()
+print(faturamento_total_por_filial)
