c4115746bea302e74a743d8376aa17857ccf920b
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-12 11:22:16 -0700

    feat: add series only

---
+++ pandas/02_series_e_dataframe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+labels = ['a', 'b', 'c']
+minha_lista = [10, 20, 30]
+arr = np.array([10, 20, 30])
+dic = {'a': 10, 'b': 20, 'c': 30}
+mostrar = pd.Series(labels)
+print(mostrar)
