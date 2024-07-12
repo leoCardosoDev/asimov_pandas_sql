f14a7375ffec37c940bca5222478dab45c0e420b
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-12 13:32:30 -0700

    feat: add concepts of dataframe

---
+++ pandas/03_dataframe.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from numpy.random import randn
+
+# DataFrame
+df = pd.DataFrame(randn(5,4), index=['A', 'B', 'C', 'D', 'E'], columns='W X Y Z'.split())
+print(df)
+print(type(df))
+print(df['W'])
+print(df[['W']])
+
+df['new'] = df['W'] + df['Y']
+print(df)
+print(df[['new']])
+df2 = df.drop('new', axis=1)
+print(df)
+df.drop('new', axis=1, inplace=True)
+print(df)
+print(df.loc['A'])
+print(df.loc[['A']])
+print(df.iloc[0, 2])
