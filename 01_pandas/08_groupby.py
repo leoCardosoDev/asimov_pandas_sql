14ec41adc4c88aaf60f493e1cee5fd9b73fa265a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-19 12:25:23 -0700

    feat: create example

---
+++ 01_pandas/08_groupby.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Dados de exemplo
+data = {
+    'Classe': ['Júnior', 'Júnior', 'Pleno', 'Pleno', 'Sênior', 'Sênior'],
+    'Nome': ['Jorge', 'Carlos', 'Roberta', 'Patrícia', 'Bruno', 'Vera'],
+    'Venda': [200, 120, 340, 124, 243, 350],
+}
+df = pd.DataFrame(data)
+print(df)
+
+print()
+print("**"*25)
+print()
+
+
+
+
+
+
