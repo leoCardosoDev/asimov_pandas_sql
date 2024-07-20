3edb942ba039b63b4f457e1e8439f69c002eab6b
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-20 11:25:54 -0700

    feat: simple operations

--- 01_pandas/10_operacoes_data_frame.py
+++ 01_pandas/10_operacoes_data_frame.py
@@ -16,3 +16,17 @@ print(f'{df["col1"].apply(computacao)}\n')
 df['col1_calc'] = df['col1'].apply(computacao)
 print(f'\n{df['col1_calc']}\n')
 print(f'\n{df['col2'].apply(lambda x: x ** 2 + 3)}\n')
+
+print(f'Soma: {df['col1'].sum()}')
+print(f'Media: {df['col1'].mean()}')
+print(f'Produto: {df['col1'].product()}')
+print(f'Desvio padrão: {df['col1'].std()}')
+print(f'Máximo: {df['col1'].max()}')
+print(f'Minimo: {df['col1'].min()}')
+print(f'Indice do máximo: {df['col1'].idxmax()}')
+print(f'Indice do mínimo: {df['col1'].idxmin()}')
+
+print(f'\n{df[df['col2'] == 444]}\n')
+print(f'{df[df['col2'] == 444]['col1'].sum()}\n')
+
+print(f'\n{df.sort_values(by='col2')}\n')
