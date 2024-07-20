b5c4f4afe9bf659559357a0f1bbc2af14330f184
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-20 09:43:23 -0700

    feat: add method concat

--- 01_pandas/09_merge_concat_join.py
+++ 01_pandas/09_merge_concat_join.py
@@ -6,11 +6,13 @@ df1 = pd.DataFrame({'A': ['A0', 'A1','A2', 'A3'],
                     'D': ['D0', 'D1', 'D2', 'D3']},
                     index=[0,1,2,3])
 
+
 df2 = pd.DataFrame({'A': ['A4', 'A5','A6', 'A7'],
                     'B': ['B4', 'B5', 'B6', 'B7'],
                     'C': ['C4', 'C5', 'C6', 'C7'],
                     'D': ['D4', 'D5', 'D6', 'D7']},
                     index=[4,5,6,7])
+
 
 df3 = pd.DataFrame({'A': ['A8', 'A9','A10', 'A11'],
                     'B': ['B8', 'B9', 'B10', 'B11'],
@@ -18,4 +20,9 @@ df3 = pd.DataFrame({'A': ['A8', 'A9','A10', 'A11'],
                     'D': ['D8', 'D9', 'D10', 'D11']},
                     index=[8,9,10,11])
 
+print(f'{df1}\n')
+print(f'{df2}\n')
+print(f'{df3}\n')
 
+print(f'{pd.concat([df1, df2, df3])}\n')
+print(f'{pd.concat([df1, df2, df3], axis=1)}\n')
