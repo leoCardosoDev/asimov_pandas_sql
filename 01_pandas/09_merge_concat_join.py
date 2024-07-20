8998ab7f1b4d2b0ffb011d7de2aa1ecde30e000e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-20 10:01:14 -0700

    feat: add pd merge

--- 01_pandas/09_merge_concat_join.py
+++ 01_pandas/09_merge_concat_join.py
@@ -26,3 +26,37 @@ print(f'{df3}\n')
 
 print(f'{pd.concat([df1, df2, df3])}\n')
 print(f'{pd.concat([df1, df2, df3], axis=1)}\n')
+
+
+esquerda = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                        'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3']})
+
+direita = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                        'C': ['C0', 'C1', 'C2', 'C3'],
+                        'D': ['D0', 'D1', 'D2', 'D3']})
+
+print(f'{esquerda}\n')
+print(f'{direita}\n')
+
+print(f'{pd.merge(esquerda, direita, on="key")}\n')
+
+esquerda2 = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                        'key2': ['K0', 'K1', 'K0', 'K1'],
+                        'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3']})
+
+direita2 = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                        'key2': ['K0', 'K0', 'K0', 'K0'],
+                        'C': ['C0', 'C1', 'C2', 'C3'],
+                        'D': ['D0', 'D1', 'D2', 'D3']})
+
+print(f'Esquerda: \n{esquerda2}\n')
+print(f'Direita: \n{direita2}\n')
+
+print(f'{pd.merge(esquerda2, direita2, on=["key1", "key2"])}\n')
+# print(f'{pd.merge(esquerda2, direita2, how='cross', on=["key1", "key2"])}')
+print(f'{pd.merge(esquerda2, direita2, how='inner', on=["key1", "key2"])}\n')
+print(f'{pd.merge(esquerda2, direita2, how='left', on=["key1", "key2"])}\n')
+print(f'{pd.merge(esquerda2, direita2, how='outer', on=["key1", "key2"])}\n')
+print(f'{pd.merge(esquerda2, direita2, how='right', on=["key1", "key2"])}\n')
