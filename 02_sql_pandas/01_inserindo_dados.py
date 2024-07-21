81e1529f2e94e69c4d8778366944bccf5588bcbf
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-21 06:51:36 -0700

    feat: load datas from csv

--- 02_sql_pandas/01_inserindo_dados.py
+++ 02_sql_pandas/01_inserindo_dados.py
@@ -3,4 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 db = Path(__file__).parent / 'web.db'
+csv = Path(__file__).parent / 'bd_data.csv'
 connection = sqlite3.connect(db)
+df_data = pd.read_csv(csv, index_col=0)
+print(df_data)
