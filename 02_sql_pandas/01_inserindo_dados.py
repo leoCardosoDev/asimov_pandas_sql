2f85c4e09c241e9c2fa10b2c39dd3eaf781752d9
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-21 06:55:34 -0700

    feat: create table data and insert csv data

--- 02_sql_pandas/01_inserindo_dados.py
+++ 02_sql_pandas/01_inserindo_dados.py
@@ -6,4 +6,5 @@ db = Path(__file__).parent / 'web.db'
 csv = Path(__file__).parent / 'bd_data.csv'
 connection = sqlite3.connect(db)
 df_data = pd.read_csv(csv, index_col=0)
-print(df_data)
+df_data.index.name = 'index_name'
+df_data.to_sql('data', connection, index_label='index_name')
