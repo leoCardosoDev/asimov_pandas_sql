cdaf6fb4572f7ffda3029de1d9d31a82e6bde178
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-21 07:13:00 -0700

    feat: add create, drop table commands

--- 02_sql_pandas/01_inserindo_dados.py
+++ 02_sql_pandas/01_inserindo_dados.py
@@ -8,3 +8,9 @@ connection = sqlite3.connect(db)
 df_data = pd.read_csv(csv, index_col=0)
 df_data.index.name = 'index_name'
 df_data.to_sql('data', connection, index_label='index_name')
+
+commands = connection.cursor()
+commands.execute('CREATE TABLE products ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT, [price] INTERGER)')
+# commands.execute('DROP TABLE products')
+# commands.execute('DROP TABLE data')
+connection.commit()
