f1f2ece8a4529dd18aea4274dcfa44fa49e26f8a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-23 14:41:19 -0700

    feat: add concat in 2 dataframe

--- 03_preco_gasolina/gasolina.py
+++ 03_preco_gasolina/gasolina.py
@@ -5,5 +5,7 @@ pasta_atual = Path(__file__).parent
 
 # 1. Carregue os conjuntos de dados "gasolina_2000+.csv" em 
 # dois Dataframes diferentes e combine-os em um unico Dataframe
-df1 = pd.read_csv(pasta_atual / 'gasolina_2000+.csv')
-df2 = pd.read_csv(pasta_atual / 'gasolina_2010+.csv')
+df1 = pd.read_csv(pasta_atual / 'gasolina_2000+.csv', index_col=0)
+df2 = pd.read_csv(pasta_atual / 'gasolina_2010+.csv', index_col=0)
+df = pd.concat([df1, df2])
+
