97f9f970b47e7dc48dadc699e81461c49387a8c6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-23 14:37:47 -0700

    feat: add dataframe 2

--- 03_preco_gasolina/gasolina.py
+++ 03_preco_gasolina/gasolina.py
@@ -6,3 +6,4 @@ pasta_atual = Path(__file__).parent
 # 1. Carregue os conjuntos de dados "gasolina_2000+.csv" em 
 # dois Dataframes diferentes e combine-os em um unico Dataframe
 df1 = pd.read_csv(pasta_atual / 'gasolina_2000+.csv')
+df2 = pd.read_csv(pasta_atual / 'gasolina_2010+.csv')
