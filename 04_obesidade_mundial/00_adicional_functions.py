63f41f7fcc40933982ef4215da6f5f25cbf43afc
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-24 07:21:15 -0700

    feat: add to frame

--- 04_obesidade_mundial/00_adicional_functions.py
+++ 04_obesidade_mundial/00_adicional_functions.py
@@ -5,3 +5,8 @@ import numpy as np
 current_folder = Path(__file__).parent
 df_obesity = pd.read_csv(current_folder / 'obesity_cleaned.csv')
 print(df_obesity)
+
+#to_frame ou []
+print(type(df_obesity['Sex']))
+print(type(df_obesity[['Sex']]))
+print(type(df_obesity['Sex'].to_frame()))
