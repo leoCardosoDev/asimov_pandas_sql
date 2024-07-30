25f34269ff46d187872465c132e269b712452b57
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-30 10:37:46 -0700

    feat: add violinplot estilization

--- 06_seaborn/05_estilizacao.py
+++ 06_seaborn/05_estilizacao.py
@@ -17,3 +17,13 @@ plt.title('Boxplot da Conta Total por Dia com Estilo Whitegrid')
 plt.xlabel('Dia')
 plt.ylabel('Conta Total')
 plt.show()
+
+# Aplicando uma paleta de cores
+sns.set_palette('pastel')
+
+# Criando um violinplot para 'total_bill' categorizado por 'day' com paleta aplicada
+sns.violinplot(x='day', y='total_bill', data=tips)
+plt.title('Violinplot da Conta Total por Dia com Paleta Pastel')
+plt.xlabel('Dia')
+plt.ylabel('Conta Total')
+plt.show()
