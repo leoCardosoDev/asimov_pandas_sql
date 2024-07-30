a42e824ce9365cbde46b14a2a9d66fcef87926d7
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-30 10:27:42 -0700

    feat: add boxplot example

--- 06_seaborn/02_categoricos.py
+++ 06_seaborn/02_categoricos.py
@@ -7,3 +7,9 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # Carregando o conjunto de dados
 tips = sns.load_dataset('tips')
 
+# Criando um boxplot para a coluna 'total_bill' categorizada por 'day'
+sns.boxplot(x='day', y='total_bill', data=tips)
+plt.title('Boxplot da Conta Total por Dia')
+plt.xlabel('Dia')
+plt.ylabel('Conta Total')
+plt.show()
