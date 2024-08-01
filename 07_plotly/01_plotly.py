6e032692a609010975d9ade3b8343eaec9bdfa2e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-01 06:24:41 -0700

    feat: add plotly example

--- 07_plotly/01_plotly.py
+++ 07_plotly/01_plotly.py
@@ -1,3 +1,6 @@
 import plotly.express as px
+from pathlib import Path
+
+folder = Path(__file__).parent
 fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
-fig.write_html('first_figure.html', auto_open=True)
+fig.write_html(folder / 'first_figure.html', auto_open=True)
