b403cac4d4d20d5dff5bb673c97d4d9fee3c4c15
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-02 12:43:08 -0700

    refactor: piechar

--- 07_plotly/02_tipos_graficos/01_basicos/03_piechar/example_1.py
+++ 07_plotly/02_tipos_graficos/01_basicos/03_piechar/example_1.py
@@ -1,10 +1,10 @@
 import plotly.graph_objects as go
 
-labels = ['Oxigênio', 'Hidrgênio', 'Gás Carbônico', 'Nitrogênio']
+labels = ['Oxigênio', 'Hidrogênio', 'Gás Carbônico', 'Nitrogênio']
 values = [4500, 2500, 1053, 500]
 
 fig = go.Figure(
-    data=go.Pie(labels=labels, values=values, pull=[0, 0, 0.02, 0])
+    data=go.Pie(labels=labels, values=values, pull=[0, 0, 0.03, 0])
 )
 
 fig.update_traces(hoverinfo="label+value")
