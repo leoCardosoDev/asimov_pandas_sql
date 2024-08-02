d64c8fa7a0ec123f4d8992c99b042335837eec10
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-02 12:05:06 -0700

    feat: add example heatmap

---
+++ 07_plotly/02_tipos_graficos/02_estatisticas/03_histogram2d_heatmap/2_heatmap_example.py
@@ -0,0 +1,8 @@
+import plotly.graph_objects as go
+
+fig = go.Figure(data=go.Heatmap(
+    z=[[1, None, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+    x=['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'],
+    y=['Manhã', 'Tarde', 'Noite'],
+))
+fig.show()
