3dba5dc88c4bd506265ad7330de6a596aa0d9a50
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-07 11:22:24 -0700

    feat: add read

---
+++ 09_projeto_sql_alchemy_crud/read.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, String, Boolean, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+# Detalhes da conexão com o PostgreSQL
+POSTGRES_USER = 'admin'
+POSTGRES_PASSWORD = '12345'
+POSTGRES_DB = 'bd_usuarios'
+POSTGRES_HOST = 'postgres'  # Nome do serviço PostgreSQL
+POSTGRES_PORT = '5432'
+
+class Base(DeclarativeBase):
+    pass
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30))
+    senha: Mapped[str] = mapped_column(String(30))
+    acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
+
+    def __repr__(self):
+        return f'Usuario({self.id}, {self.nome})'
+
+# String de conexão com o PostgreSQL
+DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(bind=engine)
+
+# CRUD
+def create_usuario(nome, senha, email, **kwargs):
+    with Session(bind=engine) as session:
+        usuario = Usuario(nome=nome, email=email, senha=senha, **kwargs)
+        session.add(usuario)
+        session.commit()
+
+def read_usuario():
+    with Session(bind=engine) as session:
+        cmd_sql = select(Usuario)
+        usuarios = session.execute(cmd_sql).fetchall()
+        users = [user[0] for user in usuarios]
+        return users
+
+def read_user_by_id(id):
+    with Session(bind=engine) as session:
+        cmd_sql = select(Usuario).filter_by(id=id)
+        usuario = session.execute(cmd_sql).fetchall()
+        return usuario[0][0]
+
+if __name__ == '__main__':
+    print(read_usuario())
+    print(read_user_by_id(1))
