fed1722288167b36b9389cf34da2902431de45ef
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-09 16:26:53 -0700

    feat: add modify delete

--- 09_projeto_sql_alchemy_crud/src/delete.py
+++ 09_projeto_sql_alchemy_crud/src/delete.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine, String, Boolean, select, update
+from sqlalchemy import create_engine, String, Boolean, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Detalhes da conexão com o PostgreSQL
 POSTGRES_USER = 'admin'
@@ -16,14 +18,23 @@ class Usuario(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     nome: Mapped[str] = mapped_column(String(30))
     email: Mapped[str] = mapped_column(String(30))
-    senha: Mapped[str] = mapped_column(String(30))
+    senha: Mapped[str] = mapped_column(String(128))
     acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
 
     def __repr__(self):
         return f'Usuario({self.id}, {self.nome})'
 
+    def define_senha(self, senha):
+        self.senha = generate_password_hash(senha)
+
+    def verifica_senha(self, senha):
+        return check_password_hash(self.senha, senha)
+
 # String de conexão com o PostgreSQL
 DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(bind=engine)
 
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
