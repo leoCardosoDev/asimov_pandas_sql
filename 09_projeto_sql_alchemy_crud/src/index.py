63691b2ac38111ad87341ade162ac4dd3d863166
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-09 18:23:33 -0700

    feat: finalize login screen with streamlint

--- 09_projeto_sql_alchemy_crud/src/index.py
+++ 09_projeto_sql_alchemy_crud/src/index.py
@@ -1,3 +1,4 @@
+from time import sleep
 import streamlit as st
 
 from crud import read_usuario
@@ -5,9 +6,34 @@ from crud import read_usuario
 def login():
     with st.container(border=True):
         st.markdown('Bem-vindo a tela de login')
+        usuarios = read_usuario()
+        usuarios = {usuario.nome: usuario for usuario in usuarios}
+        nome_usuario = st.selectbox(
+            'Selecione o usuario',
+            list(usuarios.keys())
+        )
+        senha = st.text_input(
+            'Digite a sua senha',
+            type='password'
+        )
+        if st.button('Logar'):
+            usuario = usuarios[nome_usuario]
+            if usuario.verifica_senha(senha):
+                st.success('Login Efetuado com sucesso!')
+                st.session_state['usuario'] = usuario
+                st.session_state['logado'] = True
+                sleep(1)
+                st.rerun()
+            else:
+                st.error('Login falhou. Senha errada')
 
 def main():
-    login()
+    if not 'logado' in st.session_state:
+        st.session_state['logado'] = False
+    if not st.session_state['logado']:
+      login()
+    else:
+        st.markdown('# Bem vindo ao WebApp')
 
 if __name__ == '__main__':
     main()
